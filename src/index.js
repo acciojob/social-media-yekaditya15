262eab053d500198648179f25b33a84517f6bdc8
yekaditya11 <yekaditya11@gmail.com>
2025-02-02 12:34:23 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import store from './app/store'
+import { Provider } from 'react-redux'
 
+import { fetchUsers } from './features/users/usersSlice'
 
+import './api/server'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+store.dispatch(fetchUsers())
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
